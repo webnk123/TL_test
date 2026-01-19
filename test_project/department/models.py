c77fe40e3c2bb8e0e43774f8bccf3f33dfbe5c68
zaza <geekn751@gmail.com>
2026-01-19 16:48:10 +0300

    linter

--- test_project/department/models.py
+++ test_project/department/models.py
@@ -2,20 +2,27 @@ from django.db import models
 from django.utils import timezone
 from tree_queries.models import TreeNode
 from django.utils.translation import gettext_lazy as _
-#https://github.com/feincms/django-tree-queries
+# https://github.com/feincms/django-tree-queries
+
 
 class Department(TreeNode):
-    name = models.CharField(max_length=255, unique=True, verbose_name='Название департамента')
+    name = models.CharField(
+        max_length=255,
+        unique=True,
+        verbose_name='Название департамента')
 
     class Meta:
         verbose_name = "Департамент"
         verbose_name_plural = "Департаменты"
-    
+
     def __str__(self):
         return self.name
 
     def get_full_path(self):
-        return " → ".join(anc.name for anc in self.ancestors(include_self=True))
+        return " → ".join(
+            anc.name for anc in self.ancestors(
+                include_self=True))
+
 
 class Employee(models.Model):
     first_name = models.CharField(max_length=150)
@@ -24,7 +31,7 @@ class Employee(models.Model):
     position = models.CharField(max_length=200)
     hire_date = models.DateField()
     salary = models.DecimalField(max_digits=12, decimal_places=2)
-    
+
     department = models.ForeignKey(
         'Department',
         on_delete=models.CASCADE,
@@ -41,11 +48,11 @@ class Employee(models.Model):
 
     def get_full_name(self):
         parts = [self.last_name.strip()]
-        
+
         if self.first_name.strip():
             parts.append(self.first_name.strip())
-            
+
         if self.patronymic.strip():
             parts.append(self.patronymic.strip())
-            
+
         return " ".join(parts)
--- test_project/department/views.py
+++ test_project/department/views.py
@@ -2,6 +2,7 @@ from django.views.generic import TemplateView
 
 from .models import Department, Employee
 
+
 class TreeView(TemplateView):
     template_name = 'department/tree.html'
 
